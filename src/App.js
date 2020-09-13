9d14fd092806ac80924626ce7e5898cc416dccc5
jjs951213 <jjs951213@naver.com>
2020-09-13 13:16:40 +0900

    19.2.3 Loadable Components를 통한 코드 스플리팅

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { useState, Suspense } from 'react';
 import logo from './logo.svg';
 import './App.css';
-const SplitMe = React.lazy(() => import('./SplitMe'));
+import loadable from '@loadable/component';
+const SplitMe = loadable(() => import('./SplitMe'), {
+  fallback: <div>loading... </div>
+});
+
 
 
 function App() {
@@ -9,14 +13,18 @@ function App() {
   const onClick = () => {
     setVisible(true);
   };
+  
+  const onMouseOver = () => {
+    SplitMe.preload();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>Hello React!</p>
-        <Suspense fallback={<div>loading…</div>}>
-          {visible && <SplitMe />}
-        </Suspense>
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+          Hello React!</p>
+        {visible && <SplitMe />}
       </header>
     </div>
   );
@@ -25,3 +33,4 @@ function App() {
 
 
 export default App;
+
